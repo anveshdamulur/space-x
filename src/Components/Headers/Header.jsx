bec32654f1474709dae0531c868b0db115b65f62
anveshdamulur <damulurianvesh2612@gmail.com>
2021-01-10 23:26:28 +0100

    Before deploying

--- src/Components/Headers/Header.jsx
+++ src/Components/Headers/Header.jsx
@@ -4,7 +4,7 @@ export class Header extends Component{
     render(){
         return(
             <div className='header'>
-               <h1> Welcome to Space-x </h1>
+               <h1> Welcome to Space-X </h1>
             </div>
         )
     }
